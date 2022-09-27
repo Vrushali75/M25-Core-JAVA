7afc83d57522f035cbde6b14a92a8d1a7a9e572a
Vrushali <varankarvrushali07@gmail.com>
2022-09-27 12:36:26 +0500

    First Commit

---
+++ Day-1_ConnectingEclipseWithJava/src/org/tnsindia/basic/TestingEclipseForGitHub.java
@@ -0,0 +1,10 @@
+package org.tnsindia.basic;
+//Program On to Print the statement
+public class TestingEclipseForGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to TNS India Foundation");
+	}
+
+}
