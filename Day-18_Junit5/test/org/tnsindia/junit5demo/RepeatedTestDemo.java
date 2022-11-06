167794b1ddb04987b572ed161dc0479144822766
Vrushali <varankarvrushali07@gmail.com>
2022-11-06 20:40:35 +0500

    First Commit

---
+++ Day-18_Junit5/test/org/tnsindia/junit5demo/RepeatedTestDemo.java
@@ -0,0 +1,14 @@
+package org.tnsindia.junit5demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+class RepeatedTestDemo {
+
+	@RepeatedTest(5)
+	void test() {
+		System.out.println("Hello M25");
+	}
+}
